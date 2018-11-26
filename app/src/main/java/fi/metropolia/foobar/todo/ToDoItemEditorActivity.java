0c1f0ba3c732425f0b0179efad6d2109f8f6c3e3
Visa Harvey <dragonslair@gmail.com>
2018-11-26 11:25:44 +0200

    Setting up basic activities

---
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoItemEditorActivity.java
@@ -0,0 +1,13 @@
+package fi.metropolia.foobar.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDoItemEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_item_editor);
+    }
+}
---
+++ app/src/main/java/fi/metropolia/foobar/todo/ViewToDoItemActivity.java
@@ -0,0 +1,13 @@
+package fi.metropolia.foobar.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewToDoItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_to_do_item);
+    }
+}
