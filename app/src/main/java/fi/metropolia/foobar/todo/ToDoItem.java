5c9e085192c8fdd0ce0be6abd739bb43314b5dcb
Eemeli Saarinen <eemelisa@metropolia.fi>
2018-11-26 12:43:30 +0200

    First implementation of todo item

--- app/src/main/java/fi/metropolia/foobar/todo/ToDoItem.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoItem.java
@@ -1,5 +1,35 @@
 package fi.metropolia.foobar.todo;
 
 public class ToDoItem {
+    private String title;
+    private String description;
+    private boolean highlight;
+    private boolean done;
 
+    public ToDoItem(String title, String description, boolean highlight, boolean done) {
+        this.title = title;
+        this.description = description;
+        this.highlight = highlight;
+        this.done = done;
+    }
+
+    public ToDoItem(String title, String description, boolean highlight) {
+        this(title, description, highlight, false);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public boolean isHighlight() {
+        return highlight;
+    }
 }
