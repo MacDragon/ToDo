37be448823e166fd0466fdea7873488e1581c795
Eemeli Saarinen <eemelisa@metropolia.fi>
2018-11-27 09:33:55 +0200

    Commit to allow merge

--- app/src/main/java/fi/metropolia/foobar/todo/ToDoItemEditorActivity.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoItemEditorActivity.java
@@ -2,12 +2,28 @@ package fi.metropolia.foobar.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class ToDoItemEditorActivity extends AppCompatActivity {
-
+    ToDoItem dummy = new ToDoItem("Test item", "Information", true);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_item_editor);
+    }
+
+    public void onAddClick(View v) {
+        Switch highlight = (Switch) findViewById(R.id.highlight);
+        Switch done = (Switch) findViewById(R.id.done);
+
+        Boolean isHighlighted = highlight.isChecked();
+        Boolean isDone = done.isChecked();
+
+        if (isDone){
+
+        }
+
+
     }
 }
