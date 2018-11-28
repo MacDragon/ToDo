5108f760bcd215d0a24444939fb874298df80ee2
rafe <rafemmad@gmail.com>
2018-11-28 11:49:18 +0200

    implemented getToDoList method for finding the right list to be loaded.

--- app/src/main/java/fi/metropolia/foobar/todo/SelectionList.java
+++ app/src/main/java/fi/metropolia/foobar/todo/SelectionList.java
@@ -1,12 +1,46 @@
 package fi.metropolia.foobar.todo;
 
-class SelectionList {
-    private static final SelectionList ourInstance = new SelectionList();
+import android.content.Context;
 
-    static SelectionList getInstance() {
+import java.util.ArrayList;
+
+class SelectionList {
+    private static  SelectionList ourInstance;
+    private ArrayList<ToDoItemList> selectionList;
+    private Context context;
+
+    public static SelectionList getInstance() {
+
         return ourInstance;
     }
 
-    private SelectionList() {
+    private SelectionList(Context context) {
+        this.context = context;
+        selectionList = new ArrayList<ToDoItemList>();
+        selectionList.add(new ToDoItemList("testList1", context));
     }
+
+    public static void createInstance(Context context){
+        ourInstance = new SelectionList(context);
+    }
+
+    public void addToDoList(String name){
+        selectionList.add(new ToDoItemList(name, context));
+    }
+
+    public ToDoItemList getToDoList(String name){
+        //for function to search through the array list to get the right list back.
+        // return null if no list found
+        for(ToDoItemList currentList : selectionList){
+            if (currentList.getListName().equals(name)){
+                return currentList;
+            }
+        }
+        return null; //if the method has not return anything already. it will return null. 
+    }
+
+
+
+
+
 }
