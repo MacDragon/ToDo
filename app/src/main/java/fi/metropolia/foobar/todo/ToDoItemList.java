81a8db6528bd3cf956dfe48540efaad02cd53402
Visa Harvey <dragonslair@gmail.com>
2018-11-28 11:52:53 +0200

    further work on todolist activity and todoitemlist

--- app/src/main/java/fi/metropolia/foobar/todo/ToDoItemList.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoItemList.java
@@ -8,53 +8,102 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-
+/**
+ * Class to store each ToDo List
+ */
 public class ToDoItemList {
+    // ArrayList of ToDoitems, initialise to blank array at instancing
     private ArrayList<ToDoItem> toDoList = new ArrayList<ToDoItem>();
+    // name of list, which will be same as filename to store list.
     private String listName;
+    // context needed to be able to get file handle for saving, this is passed on creation.
     private Context context;
+
+    /**
+     * Return this class instances list of todo items
+     *
+     * @return
+     */
 
     public ArrayList<ToDoItem> getToDoList() {
         return toDoList;
     }
 
+    /**
+     * Adds a new item to stored list.
+     * @param toDoItem
+     */
+
     public void addItem(ToDoItem toDoItem){
         toDoList.add(toDoItem);
     }
+
+
+    /**
+     * Returns a ToDo Lists name.
+     * @return
+     */
 
     public String getListName() {
         return listName;
     }
 
+    /**
+     * Returns an individual item at specified index from ToDo List
+     * @param index
+     * @return
+     */
+
     public ToDoItem getToDoItem(int index){
         return toDoList.get(index);
     }
 
-    public boolean saveList(){
-        // commit list to storage
 
-        String filename = "testfile";
+    /**
+     * method to save list to storage
+     * @return
+     */
+    public boolean saveList(){
+        // create google gson object to convert array list into a JSON string easily.
 
         Gson gson = new Gson();
+
+        // string to hold JSON data.
+
         String jsonConvertedArrayList = gson.toJson(toDoList);
 
         Log.d(MainActivity.getTAG(), "Json data: " + jsonConvertedArrayList);
 
+
+        // create file object
+
         FileOutputStream outputStream;
 
+        // attempt to write the file to
+
         try {
-            outputStream = context.openFileOutput(filename, context.MODE_PRIVATE);
+            // attempt to open file for writing.
+            outputStream = context.openFileOutput(listName, context.MODE_PRIVATE);
+            // write out the whole string to file, as we have not seeked to end of file this will replace existing contents.
             outputStream.write(jsonConvertedArrayList.getBytes());
             outputStream.close();
+            return true; // file saved successfully
         } catch (Exception e) {
             e.printStackTrace();
             Log.d(MainActivity.getTAG(), "Exception ");
         }
 
-        return false;
-
+        return false; // file did not save
     }
 
+
+    /**
+     * Load ( or create if it doesn't already exist ) a new list.
+     * ListName must not be blank.
+     *
+     * @param listName name for list to load or create, must not be empty.
+     * @param context
+     */
 
     public ToDoItemList(String listName, Context context){
 
--- app/src/main/java/fi/metropolia/foobar/todo/ToDoListActivity.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoListActivity.java
@@ -11,9 +11,20 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+/**
+ * Activity to show a todolist called from main selector activity
+ */
+
 public class ToDoListActivity extends AppCompatActivity {
 
     ToDoItemList toDoItemList;
+    Parcelable state = null;
+
+    /**
+     * Returns the listrow adapter saved earlier for data refresh
+     *
+     * @return
+     */
 
     public ToDoListRowAdapter getAdapter() {
         return adapter;
@@ -21,53 +32,85 @@ public class ToDoListActivity extends AppCompatActivity {
 
     private ToDoListRowAdapter adapter;
 
+    /**
+     * Setup ToDoList when activity called, using extra info to read list name.
+     *
+     * @param savedInstanceState
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
 
-        //create dummy item
+        //create dummy list and items
+
+        toDoItemList = new ToDoItemList("testlist", this);
+
         ToDoItem toDoEntry = new ToDoItem("Test", "Nothing", false, false );
-
-       toDoItemList = new ToDoItemList("testlist", this);
-       toDoItemList.addItem(toDoEntry);
-
+        toDoItemList.addItem(toDoEntry);
         toDoItemList.addItem( new ToDoItem("Test2", "Nothing", false, false ) );
 
         Log.d(MainActivity.getTAG(), "onCreate: "+ toDoItemList);
 
-       adapter = new ToDoListRowAdapter(this, R.layout.todo_item_row_layout, toDoItemList);
-       final ListView listView = (ListView) findViewById(R.id.toDoListView);
+        // adapter is saved so that we can tell it to update it's data later.
+        adapter = new ToDoListRowAdapter(this, R.layout.todo_item_row_layout, toDoItemList);
+        final ListView listView = (ListView) findViewById(R.id.toDoListView);
 
-       listView.setAdapter(adapter);
+        listView.setAdapter(adapter);
+
+
+        /**
+         * set up short press detector to launch viewer
+         */
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.d(MainActivity.getTAG(), "onItemClick(" + i + ")");
                 Intent nextActivity = new Intent(ToDoListActivity.this, ViewToDoItemActivity.class);
+                // pass viewer the listname and index
                 nextActivity.putExtra("ToDoItemIndex", i);
                 nextActivity.putExtra("ToDoListName", toDoItemList.getListName());
-                Parcelable state = listView.onSaveInstanceState();
+                state = listView.onSaveInstanceState();
                 startActivity(nextActivity);
             }
         });
 
+        /**
+         *  set up long press detector to launch editor
+         */
 
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.d(MainActivity.getTAG(), "onItemClick(" + i + ")");
                 Intent nextActivity = new Intent(ToDoListActivity.this, ToDoItemEditorActivity.class);
+                // pass editor the listname and index
                 nextActivity.putExtra("ToDoItemIndex", i);
                 nextActivity.putExtra("ToDoListName", toDoItemList.getListName());
-                Parcelable state = listView.onSaveInstanceState();
+                state = listView.onSaveInstanceState();
                 startActivity(nextActivity);
                 return true;
             }
 
-
         });
 
+    }
+
+    /**
+     *  Overridden to restore listview status, so that we return to same position after editing/viewing item
+     *  also notifies list to updatedata incase it has changed. Not ideal as always called whether data changed
+     *  or not, but keeps things simple.
+     */
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ListView listView = (ListView) findViewById(R.id.toDoListView);
+        getAdapter().notifyDataSetChanged();  // request listview updates it's content data incase it has been edited before resuming
+        if (state != null) {  // if we have a saved state, then restore it
+     //       listView.onRestoreInstanceState(state);
+            Log.d(MainActivity.getTAG(), "onResume: restore list status");}
 
 
     }
