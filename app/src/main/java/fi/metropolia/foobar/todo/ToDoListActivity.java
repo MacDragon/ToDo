f946480c19bdcee3b926e0839ce03d701bdf6445
Visa Harvey <dragonslair@gmail.com>
2018-11-29 09:46:52 +0200

    Added commentation, removed listview state saving. No longer needed after changing launchmode to singletop

--- app/src/main/java/fi/metropolia/foobar/todo/ToDoListActivity.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoListActivity.java
@@ -19,7 +19,9 @@ import java.util.ArrayList;
 public class ToDoListActivity extends AppCompatActivity {
 
     private ToDoItemList toDoItemList;
-    private Parcelable state = null;
+    private ToDoListRowAdapter adapter;
+
+  //  private Parcelable state = null;
 
     /**
      * Returns the listrow adapter saved earlier for data refresh
@@ -32,18 +34,28 @@ public class ToDoListActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Handler to respond to floating addItem button to call edit activity with request to create a new item
+     * using -1 as index to indicate that we are not editing existing item.
+     *
+     * @param view
+     */
+
     public void addClick(View view){
+        // create the intent object to call editor activity
         Intent nextActivity = new Intent(ToDoListActivity.this, ToDoItemEditorActivity.class);
+        // set extras informatino to specify the list name so editor can use right list
+        // also pass -1 to indicate we are creating an item
         nextActivity.putExtra("ToDoItemIndex", -1);
         nextActivity.putExtra("ToDoListName", toDoItemList.getListName());
-        final ListView listView = (ListView) findViewById(R.id.toDoListView);
-        state = listView.onSaveInstanceState();
+        // retrieve the listview handle so that we can save it's state
+    //    final ListView listView = (ListView) findViewById(R.id.toDoListView);
+        // store the current state of listview, so that it can be restored after
+    //    state = listView.onSaveInstanceState();
         // activity is expecting to be returned to, so start as such.
         //startActivityForResult(nextActivity);
         startActivity(nextActivity);
     }
-
-    private ToDoListRowAdapter adapter;
 
     /**
      * Setup ToDoList when activity called, using extra info to read list name.
@@ -55,26 +67,46 @@ public class ToDoListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // specifity the activities layout file
         setContentView(R.layout.activity_to_do_list);
 
         //create dummy list and items
 
+        // retrieve the extras information in order to get the passed listname to display
         Bundle extras = getIntent().getExtras();
-        // extras failing when returning from editor
+        // extras failing when returning from editor - worked around by using launchmode singletop
         String listName = extras.getString("listName");
-        Log.d(MainActivity.getTAG(), "onCreate: " + listName);
-        Log.d(MainActivity.getTAG(), "onCreate list: " + SelectionList.getInstance());
 
+      //  Log.d(MainActivity.getTAG(), "onCreate: " + listName);
+      //  Log.d(MainActivity.getTAG(), "onCreate list: " + SelectionList.getInstance());
+
+        //set the 'title bar' to the listName, so that we know what list we are displaying.
         getSupportActionBar().setTitle(listName);
+
+        // get the actual list object so we can work with it.
         toDoItemList = SelectionList.getInstance().getToDoList(listName);
 
-        ToDoItem toDoEntry = new ToDoItem("Test", "Nothing", false, false);
-        toDoItemList.addItem(toDoEntry);
+        // temporary dummy items to allow quick testing, till saving and loading is working, no need to save the variable locally
+        toDoItemList.addItem(new ToDoItem("Test", "Nothing", false, false));
         toDoItemList.addItem(new ToDoItem("Test2", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test3", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test4", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test5", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test6", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test7", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test8", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test9", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test10", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test11", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test12", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test13", "Nothing", false, false));
+        toDoItemList.addItem(new ToDoItem("Test14", "Nothing", false, false));
 
-        Log.d(MainActivity.getTAG(), "onCreate: " + toDoItemList);
 
-        // adapter is saved so that we can tell it to update it's data later.
+    //    Log.d(MainActivity.getTAG(), "onCreate: " + toDoItemList);
+
+        // handle to adapter is saved so that we can tell it to update data later from onResume..
         adapter = new ToDoListRowAdapter(this, R.layout.todo_item_row_layout, toDoItemList);
 
         final ListView listView = (ListView) findViewById(R.id.toDoListView);
@@ -93,7 +125,7 @@ public class ToDoListActivity extends AppCompatActivity {
                 // pass viewer the listname and index
                 nextActivity.putExtra("ToDoItemIndex", i);
                 nextActivity.putExtra("ToDoListName", toDoItemList.getListName());
-                state = listView.onSaveInstanceState();
+          //      state = listView.onSaveInstanceState();
                 // activity is expecting to be returned to, so start as such.
                 //startActivityForResult(nextActivity);
                 startActivity(nextActivity);
@@ -112,7 +144,7 @@ public class ToDoListActivity extends AppCompatActivity {
                 // pass editor the listname and index
                 nextActivity.putExtra("ToDoItemIndex", i);
                 nextActivity.putExtra("ToDoListName", toDoItemList.getListName());
-                state = listView.onSaveInstanceState();
+         //       state = listView.onSaveInstanceState();
                 startActivity(nextActivity);
                 return true;
             }
@@ -123,19 +155,21 @@ public class ToDoListActivity extends AppCompatActivity {
 
     /**
      *  Overridden to restore listview status, so that we return to same position after editing/viewing item
-     *  also notifies list to updatedata incase it has changed. Not ideal as always called whether data changed
-     *  or not, but keeps things simple.
+     *  also notifies listview to update it's contained data incase it has changed during editing.
+     *  Not ideal as always called whether data changed or not, but keeps things simple.
      */
 
     @Override
     protected void onResume() {
         super.onResume();
+        // retrieve the ListView object
         ListView listView = (ListView) findViewById(R.id.toDoListView);
-        getAdapter().notifyDataSetChanged();  // request listview updates it's content data incase it has been edited before resuming
-        if (state != null) {  // if we have a saved state, then restore it
-     //       listView.onRestoreInstanceState(state);
-            Log.d(MainActivity.getTAG(), "onResume: restore list status");}
-
+        // request listview updates it's content data incase it has been edited before resuming
+        getAdapter().notifyDataSetChanged(); // doesn't seem to be needed now launchmode singletop
+      //  if (state != null) {  // if we have a saved state, then restore it
+    //        listView.onRestoreInstanceState(state); // no longer needed due to launchmode singletop
+      //      Log.d(MainActivity.getTAG(), "onResume: restore list status");
+       // }
 
     }
 
--- app/src/main/java/fi/metropolia/foobar/todo/ToDoListRowAdapter.java
+++ app/src/main/java/fi/metropolia/foobar/todo/ToDoListRowAdapter.java
@@ -22,12 +22,14 @@ package fi.metropolia.foobar.todo;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,8 +39,13 @@ public class ToDoListRowAdapter extends ArrayAdapter<ToDoItem> {
     ToDoItemList toDoList;
 
     private int textColor;
+    private int backGroundColor;
 
-    private void setTextFormattingByChecked(ViewHolder holder){
+    /**
+     * As the check box can cause a live change of ToDo status, formatting split into own method to avoid duplication.
+     * @param holder class holding an instance of the current listview's controls
+     */
+    private void setTextFormatting(ViewHolder holder){
         if ( holder.checkBox.isChecked() ) {
             holder.textView.setPaintFlags(holder.textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
             holder.textView.setTextColor(Color.LTGRAY);
@@ -48,54 +55,87 @@ public class ToDoListRowAdapter extends ArrayAdapter<ToDoItem> {
 
             holder.textView.setTextColor(textColor);
         }
-
     };
+
+
+    /**
+     * required method in extending ArrayAdapter to work with a dataset
+     * @return returns size of the contained ArrayList
+     */
 
     @Override
     public int getCount() {
         return toDoList.getToDoListArray().size();
     }
 
+    /**
+     * required method in extending ArrayAdapter to work with a dataset
+     * @return returns a requested specific object the ArrayAdapter is adapting.
+     */
+
     @Override
     public ToDoItem getItem(int position) {
         return toDoList.getToDoItem(position);
     }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+    /**
+     * Method to create the ListView row item's View data for Android to display.
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        final int index = position;
+      //  final int index = position; // unneccessary variable
 
+        // viewholder is not allowed to be changed in this context after being first assigned.
         final ViewHolder holder;
 
         if (convertView == null) {
 
-            // Inflate layout
+            // Inflate layout, creates the View from layout file.
             convertView = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.todo_item_row_layout, parent, false);
 
-            // Setup ViewHolder
+            // Setup ViewHolder pattern, object to hold handles to the view's controls for later manipulation.
             holder = new ViewHolder();
             holder.checkBox = (CheckBox) convertView.findViewById(R.id.check_box);
             holder.textView = (TextView) convertView.findViewById(R.id.text);
 
-            // Store ViewHolder with this row view
+            // extract default color for text, as haven't found what resource specifies it. Workaround
             textColor = holder.textView.getCurrentTextColor();
 
+            // attaches the holder object to the view to enable usage later reusage
             convertView.setTag(holder);
 
+            //attempting to get backgroundcolor
+            backGroundColor = Color.WHITE;
+         //   backGroundColor = convertView.getBackground();
+
         } else {
-            // Use cached viewHolder
+            // Use cached ViewHolder from the View instead of creating new view to allow us to access controls without using FindViewById
             holder = (ViewHolder) convertView.getTag();
         }
 
-        // Get item from  data set at the current list position
-        final ToDoItem checkItem = getItem(position);
+
+        // set background color to highlighted is current object is marked to be. This can't be changed in liveview
+        // so doesn't need to be checked in click handler
+        if(toDoList.getToDoItem(position).isHighlight())
+        {
+        // Set a background color for ListView regular row/item
+            convertView.setBackgroundColor(Color.YELLOW);
+        }
+        else
+        {
+        // Set the background color for alternate row/item
+            convertView.setBackgroundColor(backGroundColor);
+        }
+
+        // Get item from data set at the current list position to be used for initial setup and passed into click listener
+        final ToDoItem checkItem = toDoList.getToDoItem(position);//getItem(position);
 
         if (checkItem != null) {
             // Set the views to match the item from your data set
@@ -106,31 +146,38 @@ public class ToDoListRowAdapter extends ArrayAdapter<ToDoItem> {
             holder.textView.setText(checkItem.getTitle());
         }
 
-        setTextFormattingByChecked(holder);
+        // set initial text formatting to indicate status of item when view created.
+        setTextFormatting(holder);
 
+        // set up listener on the check box control of the listview row to allow live changing of the data
         holder.checkBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(MainActivity.getTAG(), String.format("checkbox onClick, isSelected: %s ", holder.checkBox.isChecked()));
-                toDoList.getToDoItem(index).setDone(holder.checkBox.isChecked());
 
+                // set the actual todolist item to match current done state from checkbox.
                 checkItem.setDone(holder.checkBox.isChecked());
 
-                setTextFormattingByChecked(holder);
-
+                // update the text formatting of the listrow to match whatever state checkbox changed into
+                setTextFormatting(holder);
             }
         });
 
+        // return the converted view to android so it can use it, and cache it to give back to adapter later.
         return convertView;
     }
 
-
+    /**
+     * Constructor for listview row adapter
+     * @param context
+     * @param resource
+     * @param toDoList the ToDolist to be displayed inside the listview.
+     */
     public ToDoListRowAdapter(Context context, int resource  , ToDoItemList toDoList) {
             super(context, resource);
             this.toDoList = toDoList;
     }
 
-    // ViewHolder acts as a cache for row views to use for setTag.
+    // ViewHolder pattern acts as a cache for views inside row view to use for setTag to be able to get back without finding by id, more efficient
     private class ViewHolder {
         TextView textView;
         CheckBox checkBox;
